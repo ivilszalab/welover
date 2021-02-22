12914d99bc4623b8d42680d5427ae37ab4b55c1e
ivilszalab <ivilszalab@gmail.com>
2021-02-22 23:45:18 +0100

    start

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ProjectController extends Controller
+{
+    // Projekt lista
+    public function index()
+    {
+    	$projects = DB::select('select * from projects');
+        return $projects;
+    }
+
+    // Projekt űrlap
+    public function show($id)
+    {
+        return "kapott id: $id";
+    }
+
+    // Projekt létrehozása
+    public function create(Request $request)
+    {
+    	$name = $request->input('projectName');
+        return $name;
+    }
+
+    // Projekt módosítása
+    public function edit($id)
+    {
+    	return "módosítva: $id";
+    }
+
+    // Projekt törlése
+    public function delete($id)
+    {
+    	return "törölve: $id";
+    }
+}
